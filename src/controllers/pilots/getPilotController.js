1304b26b662a2bde381fa3c542063773c576976c
bagusistighfar86 <bagusistighfar86@gmail.com>
2023-07-02 01:02:44 +0700

    fix response

--- src/controllers/pilots/getPilotController.js
+++ src/controllers/pilots/getPilotController.js
@@ -1,6 +1,11 @@
 import { PilotModel } from "../../models/Pilots.js"
 
 const getPilotController = async (req, res) => {
+    let response = {
+        code: 200,
+        message: "",
+        data: {},
+    }
     try {
         const pilots = await PilotModel.find({
             auth: {
@@ -9,9 +14,22 @@ const getPilotController = async (req, res) => {
             }
         })
 
-        return res.status(200).json(pilots)
-    } catch (error) {
-        res.status(500).json({ error: "Internal server error", detail: error.message })
+        if (pilots.length === 0) {
+            response.code = 404
+            response.message = "No data found"
+            response.data = {}
+            return res.status(404).json(response)
+        }
+
+        response.code = 200
+        response.message = "Get pilots data successfull"
+        response.data = { pilots }
+        return res.status(200).json(response)
+    } catch (e) {
+        response.code = 500
+        response.message = e.message
+        response.data = {}
+        return res.status(500).json(response)
     }
 }
 
