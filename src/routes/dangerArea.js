9b3d434ba98a28f823fe2be5660b2346a64e7444
bagusistighfar86 <bagusistighfar86@gmail.com>
2023-07-03 18:37:27 +0700

    Fix

--- src/routes/dangerArea.js
+++ src/routes/dangerArea.js
@@ -1,13 +1,9 @@
 import express from "express"
-// import { verifyTokenAdmin } from "../middleware/verifyTokenAdmin.js"
-// import checkGeofencingController from "../controllers/kml/checkGeofencingController.js"
-// import { verifyToken } from "../middleware/verifyToken.js"
 import addDangerAreaController from "../controllers/dangerArea/addDangerAreaController.js"
 
 const router = express.Router()
 
 router.post("/", addDangerAreaController)
-// router.post("/check-geofencing", verifyToken, checkGeofencingController)
 
 
 export { router as dangerAreaRouter }
