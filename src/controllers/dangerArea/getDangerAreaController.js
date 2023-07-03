d351d600670382b1ff5be9513c2feb5e194ccd1a
bagusistighfar86 <bagusistighfar86@gmail.com>
2023-07-04 00:05:03 +0700

    Add get danger area

---
+++ src/controllers/dangerArea/getDangerAreaController.js
@@ -0,0 +1,31 @@
+import { DangerAreaModel } from "../../models/DangerArea"
+
+const getDangerAreaController = async (req, res) => {
+    let response = {
+        code: 200,
+        message: "",
+        data: {},
+    }
+    try {
+        const area = await DangerAreaModel.find()
+
+        if (area.length === 0) {
+            response.code = 404
+            response.message = "No data found"
+            response.data = {}
+            return res.status(404).json(response)
+        }
+
+        response.code = 200
+        response.message = "Get drones data successfull"
+        response.data = { dangerArea: area }
+        return res.status(200).json(response)
+    } catch (e) {
+        response.code = 500
+        response.message = e.message
+        response.data = {}
+        return res.status(500).json(response)
+    }
+}
+
+export default getDangerAreaController
--- src/routes/dangerArea.js
+++ src/routes/dangerArea.js
@@ -2,9 +2,11 @@ import express from "express"
 import addDangerAreaController from "../controllers/dangerArea/addDangerAreaController.js"
 import checkZoneController from "../controllers/dangerArea/checkZoneController.js"
 import { verifyToken } from "../middleware/verifyToken.js"
+import getDangerAreaController from "../controllers/dangerArea/getDangerAreaController.js"
 
 const router = express.Router()
 
+router.get("/", verifyToken, getDangerAreaController)
 router.post("/", addDangerAreaController)
 router.post("/check-zone", verifyToken, checkZoneController)
 
