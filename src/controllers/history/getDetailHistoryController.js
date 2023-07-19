f1837f7bb76eb9ace3c87705189350ce2a920753
bagusistighfar86 <bagusistighfar86@gmail.com>
2023-07-19 12:39:44 +0700

    Fix Get detail history response

--- src/controllers/history/getDetailHistoryController.js
+++ src/controllers/history/getDetailHistoryController.js
@@ -1,6 +1,11 @@
 import { HistoryModel } from "../../models/Histories.js"
 
 const getDetailHistoryController = async (req, res) => {
+    let response = {
+        code: 200,
+        message: "",
+        data: {},
+    }
     try {
         const { id } = req.params
         const history = await HistoryModel.findOne({
@@ -14,9 +19,15 @@ const getDetailHistoryController = async (req, res) => {
             return res.status(404).json({ error: 'History not found' })
         }
 
-        return res.status(200).json(history)
-    } catch (error) {
-        res.status(500).json({ error: "Internal server error", detail: error.message })
+        response.code = 200
+        response.message = "Get Detail History Success"
+        response.data = {history}
+        return res.status(200).json(response)
+    } catch (e) {
+        response.code = 500
+        response.message = e.message
+        response.data = {}
+        return res.status(500).json(response)
     }
 }
 
